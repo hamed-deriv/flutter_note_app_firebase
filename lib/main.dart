9032454d8c9997457450b3a8c83a12fe48e00b04
Hamed Rezaee <13097270+hamed-rezaee@users.noreply.github.com>
2022-08-20 19:01:13 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        home: const Text('Flutter Demo Home Page'),
+      );
+}
